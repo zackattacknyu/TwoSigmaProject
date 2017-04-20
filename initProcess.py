c602a8376b36a59d7a8a8db7f7b1c6263742cc41
zackattacknyu <zackattacknyu@gmail.com>
2017-04-20 02:57:00 -0700

    ok

---
+++ initProcess.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn import cross_validation
+import xgboost as xgb
+
+
+folder = 'dataSets/'
+trainX = np.load(folder + 'trainXarray.npy')
+trainY = np.load(folder + 'trainYarray.npy')
+testX = np.load(folder + 'testXarray.npy')
+testID = np.load(folder + 'testIDarray.npy')
+
+print(trainX.shape)
+
+
+trn_x, val_x, trn_y, val_y = cross_validation.train_test_split(
+    trainX, trainY, random_state=42, stratify=trainY,test_size=0.20)
+
+clf = xgb.XGBRegressor(max_depth=10,
+                           n_estimators=1500,
+                           min_child_weight=9,
+                           learning_rate=0.05,
+                           nthread=8,
+                           subsample=0.80,
+                           colsample_bytree=0.80,
+                            objective='multi:softprob',
+                            num_class=3,
+                           seed=4242)
+
+clf.fit(trn_x, trn_y, eval_set=[(val_x, val_y)], verbose=True,
+        eval_metric='mlogloss', early_stopping_rounds=100)
+
+pred2 = clf.predict(testX,output_margin=True)
+np.save('tempPred.npy',pred2)
---
+++ initScript.py
@@ -0,0 +1,49 @@
+import json
+import numpy as np
+
+fileNtest = 'test.json'
+fileNtrain = 'train.json'
+json_data_test=open(fileNtest).read()
+json_data_train=open(fileNtrain).read()
+
+dataTrain = json.loads(json_data_train)
+dataTest = json.loads(json_data_test)
+
+
+#Initial try will only use the following fields:
+#   Latitude, Longitude, Price, Bedrooms, Bathrooms
+def obtainNParray(data,hasY):
+    ind = 0
+    numPts = len(data['listing_id'].keys())
+    arrX = np.zeros((numPts, 5))
+    arrY = np.zeros((numPts))
+    arrListID = []
+    for idKey in data['listing_id'].keys():
+        arrX[ind, 0] = data['latitude'][idKey]
+        arrX[ind, 1] = data['longitude'][idKey]
+        arrX[ind, 2] = data['longitude'][idKey]
+        arrX[ind, 3] = data['longitude'][idKey]
+        arrX[ind, 4] = data['longitude'][idKey]
+        arrListID.append(data['listing_id'][idKey])
+        ind = ind+1
+    if hasY:
+        ind=0
+        for idKey in data['listing_id'].keys():
+            curYtext = data['interest_level'][idKey]
+            if(curYtext=='low'):
+                arrY[ind] = 1
+            elif(curYtext=='medium'):
+                arrY[ind]=2
+            else:
+                arrY[ind]=3
+            ind = ind + 1
+    return arrX,arrY,arrListID
+
+
+trainX,trainY,trainIDs = obtainNParray(dataTrain,True)
+testX,testY,testIDs = obtainNParray(dataTest,False)
+
+np.save('trainXarray.npy',trainX)
+np.save('trainYarray.npy',trainY)
+np.save('testXarray.npy',testX)
+np.save('testIDarray.npy',testIDs)
